f605b81c81254f91714219de1deb3763b2486c36
Noel Rojas <noelarojasgalviz@gmail.com>
2024-08-16 17:04:01 -0400

    adding some styles for header and footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
